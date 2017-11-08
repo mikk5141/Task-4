5cbffc4cf9c13c8e707383708b3ec978144ff2ab
Flemming Christensen <flamekfor@gmail.com>
2017-11-08 11:39:26 +0100

    Add OkHttp and GSON
    The Http client gets a string returned that will be convertet to Gson then Json object that will be parsed to the Recycler veiw.

--- app/src/main/java/com/example/mongia/task4private/MainActivity.java
+++ app/src/main/java/com/example/mongia/task4private/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.mongia.task4private;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    Gson gson = new Gson();
 
     String searchText;
     private TextView tv;
     private String url = "https://api.github.com/search/repositories?q=";
+
+
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +48,14 @@ public class MainActivity extends AppCompatActivity {
             public boolean onQueryTextSubmit(String query) {
                 searchText = query;
                 url = url + "[" + searchText + "]";
+                try {
+                    // The Respond method
+                    String getResponse = doGetRequest(url);
+                    JsonObject json = new Gson().fromJson(getResponse, JsonObject.class);
+                    // parse the the json object to Recyler veiw
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 tv.setText(url);
                 url = "https://api.github.com/search/repositories?q=";
                 return false;
@@ -43,4 +67,23 @@ public class MainActivity extends AppCompatActivity {
         });
         return super.onCreateOptionsMenu(menu);
     }
+
+    /********* OkHttpClient ***********************/
+
+    //avoid creating serveral instances, should be singleton.
+    OkHttpClient client = new OkHttpClient();
+
+    // code Request
+    String doGetRequest(String url) throws IOException{
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        return response.body().string();
+    }
+
+
+
+    /********************************************/
 }
